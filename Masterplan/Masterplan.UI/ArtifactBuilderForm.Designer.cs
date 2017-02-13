c67c1d322a25b437b97f830198eafa5119adb019
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-02-13 23:01:37 +1100

    Created another .Designer.cs

---
+++ Masterplan/Masterplan.UI/ArtifactBuilderForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Masterplan.UI
+{
+    partial class ArtifactBuilderForm
+    {
+        private IContainer components;
+
+        private Panel BtnPnl;
+
+        private Button CancelBtn;
+
+        private Button OKBtn;
+
+        private WebBrowser StatBlockBrowser;
+
+        private ToolStrip Toolbar;
+
+        private ToolStripDropDownButton FileMenu;
+
+        private ToolStripMenuItem FileImport;
+
+        private ToolStripMenuItem FileExport;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && this.components != null)
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(ArtifactBuilderForm));
+            this.BtnPnl = new Panel();
+            this.CancelBtn = new Button();
+            this.OKBtn = new Button();
+            this.StatBlockBrowser = new WebBrowser();
+            this.Toolbar = new ToolStrip();
+            this.FileMenu = new ToolStripDropDownButton();
+            this.FileImport = new ToolStripMenuItem();
+            this.FileExport = new ToolStripMenuItem();
+            this.BtnPnl.SuspendLayout();
+            this.Toolbar.SuspendLayout();
+            base.SuspendLayout();
+            this.BtnPnl.Controls.Add(this.CancelBtn);
+            this.BtnPnl.Controls.Add(this.OKBtn);
+            this.BtnPnl.Dock = DockStyle.Bottom;
+            this.BtnPnl.Location = new Point(0, 443);
+            this.BtnPnl.Name = "BtnPnl";
+            this.BtnPnl.Size = new Size(384, 35);
+            this.BtnPnl.TabIndex = 2;
+            this.CancelBtn.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right);
+            this.CancelBtn.DialogResult = DialogResult.Cancel;
+            this.CancelBtn.Location = new Point(297, 6);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new Size(75, 23);
+            this.CancelBtn.TabIndex = 1;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.OKBtn.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right);
+            this.OKBtn.DialogResult = DialogResult.OK;
+            this.OKBtn.Location = new Point(216, 6);
+            this.OKBtn.Name = "OKBtn";
+            this.OKBtn.Size = new Size(75, 23);
+            this.OKBtn.TabIndex = 0;
+            this.OKBtn.Text = "OK";
+            this.OKBtn.UseVisualStyleBackColor = true;
+            this.StatBlockBrowser.AllowWebBrowserDrop = false;
+            this.StatBlockBrowser.Dock = DockStyle.Fill;
+            this.StatBlockBrowser.IsWebBrowserContextMenuEnabled = false;
+            this.StatBlockBrowser.Location = new Point(0, 25);
+            this.StatBlockBrowser.MinimumSize = new Size(20, 20);
+            this.StatBlockBrowser.Name = "StatBlockBrowser";
+            this.StatBlockBrowser.ScriptErrorsSuppressed = true;
+            this.StatBlockBrowser.Size = new Size(384, 418);
+            this.StatBlockBrowser.TabIndex = 2;
+            this.StatBlockBrowser.WebBrowserShortcutsEnabled = false;
+            this.StatBlockBrowser.Navigating += new WebBrowserNavigatingEventHandler(this.Browser_Navigating);
+            this.Toolbar.Items.AddRange(new ToolStripItem[]
+            {
+                this.FileMenu
+            });
+            this.Toolbar.Location = new Point(0, 0);
+            this.Toolbar.Name = "Toolbar";
+            this.Toolbar.Size = new Size(384, 25);
+            this.Toolbar.TabIndex = 0;
+            this.Toolbar.Text = "toolStrip1";
+            this.FileMenu.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            this.FileMenu.DropDownItems.AddRange(new ToolStripItem[]
+            {
+                this.FileImport,
+                this.FileExport
+            });
+            this.FileMenu.Image = (Image)resources.GetObject("FileMenu.Image");
+            this.FileMenu.ImageTransparentColor = Color.Magenta;
+            this.FileMenu.Name = "FileMenu";
+            this.FileMenu.Size = new Size(38, 22);
+            this.FileMenu.Text = "File";
+            this.FileImport.Name = "FileImport";
+            this.FileImport.Size = new Size(119, 22);
+            this.FileImport.Text = "Import...";
+            this.FileImport.Click += new EventHandler(this.FileImport_Click);
+            this.FileExport.Name = "FileExport";
+            this.FileExport.Size = new Size(119, 22);
+            this.FileExport.Text = "Export...";
+            this.FileExport.Click += new EventHandler(this.FileExport_Click);
+            base.AcceptButton = this.OKBtn;
+            base.AutoScaleDimensions = new SizeF(6f, 13f);
+            base.AutoScaleMode = AutoScaleMode.Font;
+            base.CancelButton = this.CancelBtn;
+            base.ClientSize = new Size(384, 478);
+            base.Controls.Add(this.StatBlockBrowser);
+            base.Controls.Add(this.BtnPnl);
+            base.Controls.Add(this.Toolbar);
+            base.MaximizeBox = false;
+            base.MinimizeBox = false;
+            base.Name = "ArtifactBuilderForm";
+            base.ShowIcon = false;
+            base.ShowInTaskbar = false;
+            base.StartPosition = FormStartPosition.CenterParent;
+            this.Text = "Artifact Builder";
+            this.BtnPnl.ResumeLayout(false);
+            this.Toolbar.ResumeLayout(false);
+            this.Toolbar.PerformLayout();
+            base.ResumeLayout(false);
+            base.PerformLayout();
+        }
+
+    }
+}
--- Masterplan/Masterplan.UI/ArtifactBuilderForm.cs
+++ Masterplan/Masterplan.UI/ArtifactBuilderForm.cs
@@ -8,26 +8,8 @@ using Utils;
 
 namespace Masterplan.UI
 {
-	internal class ArtifactBuilderForm : Form
+	internal partial class ArtifactBuilderForm : Form
 	{
-		private IContainer components;
-
-		private Panel BtnPnl;
-
-		private Button CancelBtn;
-
-		private Button OKBtn;
-
-		private WebBrowser StatBlockBrowser;
-
-		private ToolStrip Toolbar;
-
-		private ToolStripDropDownButton FileMenu;
-
-		private ToolStripMenuItem FileImport;
-
-		private ToolStripMenuItem FileExport;
-
 		private Artifact fArtifact;
 
 		public Artifact Artifact
@@ -38,118 +20,11 @@ namespace Masterplan.UI
 			}
 		}
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && this.components != null)
-			{
-				this.components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-
-		private void InitializeComponent()
-		{
-			ComponentResourceManager resources = new ComponentResourceManager(typeof(ArtifactBuilderForm));
-			this.BtnPnl = new Panel();
-			this.CancelBtn = new Button();
-			this.OKBtn = new Button();
-			this.StatBlockBrowser = new WebBrowser();
-			this.Toolbar = new ToolStrip();
-			this.FileMenu = new ToolStripDropDownButton();
-			this.FileImport = new ToolStripMenuItem();
-			this.FileExport = new ToolStripMenuItem();
-			this.BtnPnl.SuspendLayout();
-			this.Toolbar.SuspendLayout();
-			base.SuspendLayout();
-			this.BtnPnl.Controls.Add(this.CancelBtn);
-			this.BtnPnl.Controls.Add(this.OKBtn);
-			this.BtnPnl.Dock = DockStyle.Bottom;
-			this.BtnPnl.Location = new Point(0, 443);
-			this.BtnPnl.Name = "BtnPnl";
-			this.BtnPnl.Size = new Size(384, 35);
-			this.BtnPnl.TabIndex = 2;
-			this.CancelBtn.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right);
-			this.CancelBtn.DialogResult = DialogResult.Cancel;
-			this.CancelBtn.Location = new Point(297, 6);
-			this.CancelBtn.Name = "CancelBtn";
-			this.CancelBtn.Size = new Size(75, 23);
-			this.CancelBtn.TabIndex = 1;
-			this.CancelBtn.Text = "Cancel";
-			this.CancelBtn.UseVisualStyleBackColor = true;
-			this.OKBtn.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right);
-			this.OKBtn.DialogResult = DialogResult.OK;
-			this.OKBtn.Location = new Point(216, 6);
-			this.OKBtn.Name = "OKBtn";
-			this.OKBtn.Size = new Size(75, 23);
-			this.OKBtn.TabIndex = 0;
-			this.OKBtn.Text = "OK";
-			this.OKBtn.UseVisualStyleBackColor = true;
-			this.StatBlockBrowser.AllowWebBrowserDrop = false;
-			this.StatBlockBrowser.Dock = DockStyle.Fill;
-			this.StatBlockBrowser.IsWebBrowserContextMenuEnabled = false;
-			this.StatBlockBrowser.Location = new Point(0, 25);
-			this.StatBlockBrowser.MinimumSize = new Size(20, 20);
-			this.StatBlockBrowser.Name = "StatBlockBrowser";
-			this.StatBlockBrowser.ScriptErrorsSuppressed = true;
-			this.StatBlockBrowser.Size = new Size(384, 418);
-			this.StatBlockBrowser.TabIndex = 2;
-			this.StatBlockBrowser.WebBrowserShortcutsEnabled = false;
-			this.StatBlockBrowser.Navigating += new WebBrowserNavigatingEventHandler(this.Browser_Navigating);
-			this.Toolbar.Items.AddRange(new ToolStripItem[]
-			{
-				this.FileMenu
-			});
-			this.Toolbar.Location = new Point(0, 0);
-			this.Toolbar.Name = "Toolbar";
-			this.Toolbar.Size = new Size(384, 25);
-			this.Toolbar.TabIndex = 0;
-			this.Toolbar.Text = "toolStrip1";
-			this.FileMenu.DisplayStyle = ToolStripItemDisplayStyle.Text;
-			this.FileMenu.DropDownItems.AddRange(new ToolStripItem[]
-			{
-				this.FileImport,
-				this.FileExport
-			});
-			this.FileMenu.Image = (Image)resources.GetObject("FileMenu.Image");
-			this.FileMenu.ImageTransparentColor = Color.Magenta;
-			this.FileMenu.Name = "FileMenu";
-			this.FileMenu.Size = new Size(38, 22);
-			this.FileMenu.Text = "File";
-			this.FileImport.Name = "FileImport";
-			this.FileImport.Size = new Size(119, 22);
-			this.FileImport.Text = "Import...";
-			this.FileImport.Click += new EventHandler(this.FileImport_Click);
-			this.FileExport.Name = "FileExport";
-			this.FileExport.Size = new Size(119, 22);
-			this.FileExport.Text = "Export...";
-			this.FileExport.Click += new EventHandler(this.FileExport_Click);
-			base.AcceptButton = this.OKBtn;
-			base.AutoScaleDimensions = new SizeF(6f, 13f);
-			base.AutoScaleMode = AutoScaleMode.Font;
-			base.CancelButton = this.CancelBtn;
-			base.ClientSize = new Size(384, 478);
-			base.Controls.Add(this.StatBlockBrowser);
-			base.Controls.Add(this.BtnPnl);
-			base.Controls.Add(this.Toolbar);
-			base.MaximizeBox = false;
-			base.MinimizeBox = false;
-			base.Name = "ArtifactBuilderForm";
-			base.ShowIcon = false;
-			base.ShowInTaskbar = false;
-			base.StartPosition = FormStartPosition.CenterParent;
-			this.Text = "Artifact Builder";
-			this.BtnPnl.ResumeLayout(false);
-			this.Toolbar.ResumeLayout(false);
-			this.Toolbar.PerformLayout();
-			base.ResumeLayout(false);
-			base.PerformLayout();
-		}
-
 		public ArtifactBuilderForm(Artifact artifact)
 		{
 			this.InitializeComponent();
 			this.fArtifact = artifact.Copy();
-			this.update_statblock();
+			this.UpdateStatblock();
 		}
 
 		private void Browser_Navigating(object sender, WebBrowserNavigatingEventArgs e)
@@ -164,7 +39,7 @@ namespace Masterplan.UI
 					{
 						this.fArtifact.Name = artifactProfileForm.Artifact.Name;
 						this.fArtifact.Tier = artifactProfileForm.Artifact.Tier;
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 				if (e.Url.LocalPath == "description")
@@ -174,7 +49,7 @@ namespace Masterplan.UI
 					if (detailsForm.ShowDialog() == DialogResult.OK)
 					{
 						this.fArtifact.Description = detailsForm.Details;
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 				if (e.Url.LocalPath == "details")
@@ -184,7 +59,7 @@ namespace Masterplan.UI
 					if (detailsForm2.ShowDialog() == DialogResult.OK)
 					{
 						this.fArtifact.Details = detailsForm2.Details;
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 				if (e.Url.LocalPath == "goals")
@@ -194,7 +69,7 @@ namespace Masterplan.UI
 					if (detailsForm3.ShowDialog() == DialogResult.OK)
 					{
 						this.fArtifact.Goals = detailsForm3.Details;
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 				if (e.Url.LocalPath == "rp")
@@ -204,7 +79,7 @@ namespace Masterplan.UI
 					if (detailsForm4.ShowDialog() == DialogResult.OK)
 					{
 						this.fArtifact.RoleplayingTips = detailsForm4.Details;
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 			}
@@ -218,7 +93,7 @@ namespace Masterplan.UI
 					if (magicItemSectionForm.ShowDialog() == DialogResult.OK)
 					{
 						this.fArtifact.Sections.Add(magicItemSectionForm.Section);
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 				if (e.Url.LocalPath.Contains(",new"))
@@ -234,7 +109,7 @@ namespace Masterplan.UI
 						if (magicItemSectionForm2.ShowDialog() == DialogResult.OK)
 						{
 							artifactConcordance.Sections.Add(magicItemSectionForm2.Section);
-							this.update_statblock();
+							this.UpdateStatblock();
 						}
 					}
 					catch
@@ -260,7 +135,7 @@ namespace Masterplan.UI
 						if (magicItemSectionForm3.ShowDialog() == DialogResult.OK)
 						{
 							artifactConcordance2.Sections[index3] = magicItemSectionForm3.Section;
-							this.update_statblock();
+							this.UpdateStatblock();
 						}
 						goto IL_42B;
 					}
@@ -278,7 +153,7 @@ namespace Masterplan.UI
 					if (magicItemSectionForm4.ShowDialog() == DialogResult.OK)
 					{
 						this.fArtifact.Sections[index4] = magicItemSectionForm4.Section;
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 				catch
@@ -300,7 +175,7 @@ namespace Masterplan.UI
 						int index6 = int.Parse(s5);
 						ArtifactConcordance artifactConcordance3 = this.fArtifact.ConcordanceLevels[index5];
 						artifactConcordance3.Sections.RemoveAt(index6);
-						this.update_statblock();
+						this.UpdateStatblock();
 						goto IL_51B;
 					}
 					catch
@@ -313,7 +188,7 @@ namespace Masterplan.UI
 				{
 					int index7 = int.Parse(e.Url.LocalPath);
 					this.fArtifact.Sections.RemoveAt(index7);
-					this.update_statblock();
+					this.UpdateStatblock();
 				}
 				catch
 				{
@@ -330,7 +205,7 @@ namespace Masterplan.UI
 					if (artifactConcordanceForm.ShowDialog() == DialogResult.OK)
 					{
 						this.fArtifact.ConcordanceRules.Add(artifactConcordanceForm.Concordance);
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 			}
@@ -345,7 +220,7 @@ namespace Masterplan.UI
 					if (artifactConcordanceForm2.ShowDialog() == DialogResult.OK)
 					{
 						this.fArtifact.ConcordanceRules[index8] = artifactConcordanceForm2.Concordance;
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 				catch
@@ -359,7 +234,7 @@ namespace Masterplan.UI
 				{
 					int index9 = int.Parse(e.Url.LocalPath);
 					this.fArtifact.ConcordanceRules.RemoveAt(index9);
-					this.update_statblock();
+					this.UpdateStatblock();
 				}
 				catch
 				{
@@ -376,7 +251,7 @@ namespace Masterplan.UI
 					if (detailsForm5.ShowDialog() == DialogResult.OK)
 					{
 						artifactConcordance4.Quote = detailsForm5.Details;
-						this.update_statblock();
+						this.UpdateStatblock();
 					}
 				}
 				catch
@@ -394,7 +269,7 @@ namespace Masterplan.UI
 					if (detailsForm6.ShowDialog() == DialogResult.OK)
 					{
 						artifactConcordance5.Description = detailsForm6.Details;
-						this.update_statblock();
+                        UpdateStatblock();
 					}
 				}
 				catch
@@ -403,9 +278,9 @@ namespace Masterplan.UI
 			}
 		}
 
-		private void update_statblock()
+		private void UpdateStatblock()
 		{
-			this.StatBlockBrowser.DocumentText = HTML.Artifact(this.fArtifact, DisplaySize.Small, true, true);
+            StatBlockBrowser.DocumentText = HTML.Artifact(fArtifact, DisplaySize.Small, true, true);
 		}
 
 		private void FileImport_Click(object sender, EventArgs e)
@@ -415,16 +290,18 @@ namespace Masterplan.UI
 			{
 				return;
 			}
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.Title = "Import Artifact";
-			openFileDialog.Filter = Program.ArtifactFilter;
-			if (openFileDialog.ShowDialog() == DialogResult.OK)
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Title = "Import Artifact",
+                Filter = Program.ArtifactFilter
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
 			{
 				Artifact artifact = Serialisation<Artifact>.Load(openFileDialog.FileName, SerialisationMode.Binary);
 				if (artifact != null)
 				{
 					this.fArtifact = artifact;
-					this.update_statblock();
+					this.UpdateStatblock();
 					return;
 				}
 				string text2 = "The artifact could not be imported.";
